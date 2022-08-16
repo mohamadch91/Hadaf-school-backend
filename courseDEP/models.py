46221f53697ebb76ea5bcc752058b4b0e38742ef
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-17 01:22:45 +0430

    add serilizers

--- courseDEP/models.py
+++ courseDEP/models.py
@@ -1,3 +1,44 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+class department(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    sortindex=models.IntegerField(blank=True,null=True)
+    def __str__(self):
+        return self.name
+
+class grade(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    sortindex=models.IntegerField(blank=True,null=True)
+    def __str__(self):
+        return self.name
+
+class lessons(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    code=models.CharField(max_length=100)
+    department=models.ForeignKey(department,on_delete=models.CASCADE)
+    grade=models.ForeignKey(grade,on_delete=models.CASCADE)
+    sortindex=models.IntegerField(blank=True,null=True)
+    def __str__(self):
+        return self.name
+
+class days(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class year(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class CourseType(models.Model):
+    id=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
---
+++ courseDEP/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import *
+
+class departmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = department
+        fields = '__all__'
+
+class gradeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = grade
+        fields = '__all__'
+
+class lessonsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = lessons
+        fields = '__all__'
+
+class daysSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = days
+        fields = '__all__'
+
+class   yearSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = year
+        fields = '__all__'
+class CourseTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseType
+        fields = '__all__'        
