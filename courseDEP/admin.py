571f1af16ff78c3fc54fc845a6c3a448fe0ca0b5
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-18 12:29:48 +0430

    registers models to admin

--- courseDEP/admin.py
+++ courseDEP/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(department)
+admin.site.register(grade)
+admin.site.register(lessons)
+admin.site.register(days)
+admin.site.register(year)
+admin.site.register(CourseType)
