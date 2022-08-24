3ec1850007ba60b9b51346d9265dc931cb58853a
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-24 04:31:27 +0430

    add new models to adminn site for discount,forum,course,archive

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(StudetCourse)
+
+admin.site.register(CourseHomeWork)
+
+admin.site.register(CourseDays)
+admin.site.register(CourseType)
