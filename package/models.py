2af7ab84644ef791811921df0197b4e19bf30dcf
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-04 05:19:38 +0430

    change quiz models

--- package/models.py
+++ package/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from course.models import Course
+from discount.models import Discount
 # Create your models here.
+
+
+
--- quiz/models.py
+++ quiz/models.py
@@ -12,6 +12,8 @@ class quizHeader(models.Model):
     teacher=models.ForeignKey(Teacher,on_delete=models.CASCADE,blank=True,null=True)
     course=models.ForeignKey(Course,on_delete=models.Case,blank=True,null=True)
     quizTime=models.IntegerField( blank=True,null=True,default=0)
+    start_data=models.DateTimeField(blank=True,null=True)
+    end_data=models.DateTimeField(blank=True,null=True)
     question_count=models.IntegerField(blank=True,null=True,default=5)
     created_at=models.DateTimeField(auto_now_add=True,null=True)    ########
     updated_at=models.DateTimeField(auto_now=True,null=True) 
