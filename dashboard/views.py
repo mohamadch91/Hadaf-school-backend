87c381956bd54f4b5e21a8f95ce077767bf6e8e2
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-16 13:36:55 +0430

    change phone finder

--- dashboard/views.py
+++ dashboard/views.py
@@ -110,7 +110,7 @@ class basketView(APIView):
                 })
             elif x.type=='timing':
                 timing=timingPackage.objects.get(id=x.buyID)
-                course=timingPackageCourse.objects.filter(packageID=normal.id)
+                course=timingPackageCourse.objects.filter(packageID=timing.id)
                 #calculate price
                 price=0
                 for z in course:
@@ -133,7 +133,7 @@ class basketView(APIView):
                 })
             elif x.type=='student':
                 student=studentPackage.objects.get(id=x.buyID)
-                course=studentPackageCourse.objects.filter(packageID=normal.id)
+                course=studentPackageCourse.objects.filter(packageID=student.id)
                 #calculate price
                 price=0
                 count_course=course.count()
