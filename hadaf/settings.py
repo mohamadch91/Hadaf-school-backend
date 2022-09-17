5245f072a77b848bbaee0336f472f02d6bedabbc
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-18 01:03:44 +0430

    chanage db

--- hadaf/settings.py
+++ hadaf/settings.py
@@ -136,23 +136,23 @@ WSGI_APPLICATION = 'hadaf.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-#   'default': {
-#      'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'hadaf',
-#         'USER': 'hadaf',
-#         'PASSWORD': '77nZcM3onVFVrV5L*s7hex#!3XKYQ5oz',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-# }
-# }
-# 77nZcM3onVFVrV5L*s7hex#!3XKYQ5oz
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+     'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hadaf',
+        'USER': 'hadaf',
+        'PASSWORD': '77nZcM3onVFVrV5L*s7hex#!3XKYQ5oz',
+        'HOST': 'localhost',
+        'PORT': '5432',
 }
+}
+# 77nZcM3onVFVrV5L*s7hex#!3XKYQ5oz
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
