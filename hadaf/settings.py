7944105f5b288623cb48e3a78667336e6596eb5b
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-18 00:42:55 +0430

    chanage db

--- hadaf/settings.py
+++ hadaf/settings.py
@@ -136,16 +136,16 @@ WSGI_APPLICATION = 'hadaf.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-#   'default': {
-#      'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'hadaf',
-#         'USER': 'hadaf',
-#         'PASSWORD': '77nZcM3onVFVrV5L*s7hex#!3XKYQ5oz',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-# }
-# }
+DATABASES = {
+  'default': {
+     'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hadaf',
+        'USER': 'hadaf',
+        'PASSWORD': '77nZcM3onVFVrV5L*s7hex#!3XKYQ5oz',
+        'HOST': 'localhost',
+        'PORT': '5432',
+}
+}
 # 77nZcM3onVFVrV5L*s7hex#!3XKYQ5oz
 # DATABASES = {
 #     'default': {
