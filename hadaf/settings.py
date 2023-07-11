021afc32c6c07676dc21369806afbfa4f67b1caf
mohamadch91 <mohamadchoupan80@gmail.com>
2023-07-12 03:25:01 +0330

    fix: / in cros origin

--- hadaf/settings.py
+++ hadaf/settings.py
@@ -190,7 +190,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "https://www.test-cors.org",'http://localhost:3000','http://212.33.202.49','http://212.33.202.49:3000','http://212.33.202.49:3001','https://srvschool.ir','https://adm.srvschool.ir',
-    'http://localhost:3001','http://94.101.187.240:3001','http://94.101.187.240:3000/'
+    'http://localhost:3001','http://94.101.187.240:3001','http://94.101.187.240:3000'
 ]
 
 # CSRF_TRUSTED_ORIGINS = [
